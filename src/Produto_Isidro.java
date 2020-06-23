d7687db6b0daccd930c5ff85ede45e0d93014e7a
slcoutinho <slcoutinho@outlook.com>
2020-06-23 09:14:03 -0300

    Segundo Commit

---
+++ src/Produto_Isidro.java
@@ -0,0 +1,27 @@
+
+public class Produto_Isidro {
+	private int id;
+	private String nome;
+
+	public Produto_Isidro(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
